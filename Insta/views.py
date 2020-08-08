a676e8058b1fc2107848888fec6d4f0f167086a0
xzglovenk <xzglovenk@gmail.com>
2020-08-08 00:12:43 -0400

    Master/Detail view of pics

--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from Insta.models import Post
+
 
 # Create your views here.
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
